6ed3bfa74fba067433e4a70f7660d582c427201d
Intime-SA <invertimesa@gmail.com>
2024-10-20 22:06:24 -0300

    cambios merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,8 @@ import React from "react";
 import { Montserrat } from "next/font/google";
 import AdminLoginForm from "@/src/components/login/AdminLoginForm";
 
-
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-<<<<<<< HEAD
-=======
-
-interface FeatureCardProps {
-  icon: ReactElement;
-  title: string;
-  description: string;
-}
-
-function FeatureCard({ icon, title, description }: FeatureCardProps) {
-  return (
-    <div className="text-left">
-      <div className="flex items-center mb-2">
-        {React.cloneElement(icon, {
-          className: "w-6 h-6 mr-2 text-gray-800 dark:text-gray-200",
-        })}
-        <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-          {title}
-        </h2>
-      </div>
-      <p className="text-gray-600 dark:text-gray-400">{description}</p>
-    </div>
-  );
-}
-
->>>>>>> 21a6db885ccee0201fdca1a42f3bd4a851860fb5
 export default function Home() {
   return (
     <div
